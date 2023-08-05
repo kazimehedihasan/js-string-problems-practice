44154805f14d2bb3f999d7ee94cb9dccb45e604f
kazimehedihasan <kazimehedihasan458@.com>
2023-08-05 22:14:21 +0600

    first commit

---
+++ js-string-problems-practice/sum-of-numbers.js
@@ -0,0 +1,47 @@
+// 1 + 2 + 3 + 4 + 5 + 6 + 7
+
+// let sum = 0;
+// for(let i = 1; i <= 7; i++){
+//     sum = sum + i;
+//     console.log(i, sum);
+// }
+
+
+// function sumOfNumbers(number){
+//     let sum = 0;
+//     for(let i = 1; i <= number; i++){
+//         sum = sum + i;
+//         console.log(i, sum);
+//     }
+//     return sum;
+// }
+// const result = sumOfNumbers(7)
+// console.log('finel sum', result);
+
+
+
+//   !   factorial
+
+
+// function sumOfNumbers(number){
+//     let sum = 1;
+//     for(let i = 1; i <= number; i++){
+//         sum = sum * i;
+//         // console.log(i, sum);
+//     }
+//     return sum;
+// }
+// const result = sumOfNumbers(9)
+// console.log('finel sum', result);
+
+
+
+
+// --------------------------------------
+function factorial(number){
+    let result = 1;
+    for(let i = number; i >=1; i--){
+        console.log(i);
+    }
+}
+factorial(7);
